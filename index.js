2dddf32060de4809a01f5df7407cde599db49db8
rickie maweu <rickiemaweu@gmail.com>
2022-06-08 23:43:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(todo= 'roller-skate'){
+    return `This Saturday, I want to ${todo}!`
+}
+
+const mondayWork = function(todo='go to the office'){
+    return `This Monday, I will ${todo}.`    
+}
+
+function wrapAdjective(wrap= '*'){
+    return function(adj="special"){
+        return `You are ${wrap}${adj}${wrap}!`
+    }
+
+}
